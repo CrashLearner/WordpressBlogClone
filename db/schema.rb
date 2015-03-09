455a95f5434a488d96f7f7c62c5c8c154d4fc987
Yonathan Ayenew <yonayen@gmail.com>
2015-03-08 20:36:18 -0700

    Add Comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308210330) do
+ActiveRecord::Schema.define(version: 20150309023536) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
